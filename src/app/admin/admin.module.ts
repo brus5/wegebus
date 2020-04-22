9f272e7b9c01de8d10ebdc0e5b9de6bf170101ce
Łukasz Krawczak <lukasz634@gmail.com>
2020-04-22 12:38:56 +0200

    Fixing small issues

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,13 +1,11 @@
 import {NgModule} from '@angular/core';
 
-import {ProductCardWaitingComponent} from './components/product-card-waiting/product-card-waiting.component';
 import {AdminOrdersComponent} from './components/admin-orders/admin-orders.component';
 
-import {SharedModule} from '../shared/shared.module';
+import {SharedModule} from 'shared/shared.module';
 
 @NgModule({
   declarations: [
-    ProductCardWaitingComponent,
     AdminOrdersComponent
   ],
   imports: [
